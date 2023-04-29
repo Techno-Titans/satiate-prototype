976c01f6b8edafcc3c67c84bf27f290d263376f5
AJ APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-04-29 20:23:06 +0800

    Added home functionality

--- www/js/home.js
+++ www/js/home.js
@@ -1,1 +1,15 @@
-"use strict";
+const scaleButton = document.querySelector('.post-button');
+
+scaleButton.addEventListener('click', () => {
+  scaleButton.classList.add('button-anim');
+  setTimeout(() => {
+    scaleButton.classList.remove('button-anim');
+  }, 370);
+});
+
+const originalElement = document.querySelector('.element-post');
+
+scaleButton.addEventListener('click', () => {
+  const clonedElement = originalElement.cloneNode(true);
+  originalElement.parentNode.appendChild(clonedElement);
+});
